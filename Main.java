286c0ceef2630f21b9b837290df9b2950652aac1
JamesTurntz <tz_blues@163.com>
2018-04-17 17:00:57 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package tool;
+
+import java.awt.EventQueue;
+
+public class Main {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frmStyx.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
